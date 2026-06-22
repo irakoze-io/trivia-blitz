839b6e0f9528d1ec01b86c618588fef1a1ae8be4
Ivan Irakoze <134937903+irakoze-io@users.noreply.github.com>
2026-06-23 00:47:36 +0200

    Initial commit

---
+++ src/main/java/dev/irakodes/triviablitz/TriviaBlitzApplication.java
@@ -0,0 +1,13 @@
+package dev.irakodes.triviablitz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaBlitzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriviaBlitzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/irakodes/triviablitz/TriviaBlitzApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.irakodes.triviablitz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaBlitzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
